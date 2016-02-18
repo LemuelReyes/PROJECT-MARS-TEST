2702880f8a8288a700e7b4f6e2adfc739f0028a8
lemuel <reyeslemuel@hotmail.com>
2016-02-18 13:07:43 -0800

    position the digital clock to the top right

--- main.jsx
+++ main.jsx
@@ -9,10 +9,13 @@ render: function(){
      <div className="wrapper">
        <header>
          <h1>mars</h1>
+
          <div className="rocket-container">
               <i className="fa fa-space-shuttle"></i>
          </div>
        </header>
+
+      <div className="clock-button">50</div>
 
       <div className="button-flex">
         <button>Begin Evaluation</button>
