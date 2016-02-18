4a801c7c3f5497a4ecd440bdfdd86d3e76beaa23
lemuel <reyeslemuel@hotmail.com>
2016-02-18 11:22:53 -0800

    flexbox button to center

--- main.jsx
+++ main.jsx
@@ -6,22 +6,18 @@ var MARS = React.createClass ({
 render: function(){
    return (
      //this sets the color background
-     <div>
-     <body>
+     <div className="wrapper">
        <header>
          <h1>mars</h1>
-
          <div className="rocket-container">
-           <i className="fa fa-space-shuttle"></i>
+              <i className="fa fa-space-shuttle"></i>
          </div>
-
        </header>
 
-    <div className="button-flex">
-     <button>Begin Evaluation</button>
-    </div>
-     </body>
-    </div>
+      <div className="button-flex">
+        <button>Begin Evaluation</button>
+      </div>
+    </div> //ends wrapper div
 
    )
 }
